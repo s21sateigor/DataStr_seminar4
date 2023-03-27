660dd3a830f657c9263f3edefe2803fbba358e97
karina.skirmante <karina.krinkele@venta.lv>
2023-03-27 11:15:31 +0300

    Add: MyVerticeNode - variables and get and set

---
+++ src/datastr/MyVerticeNode.java
@@ -0,0 +1,29 @@
+package datastr;
+
+public class MyVerticeNode<T> {
+	private T element;
+	private MyEdgeNode firstEdge = null;
+	
+	public T getElement() {
+		return element;
+	}
+	public void setElement(T inputElement) {
+		if(inputElement!=null)
+		{
+			element = inputElement;
+		}
+		else
+		{
+			element = (T) new Object();
+		}
+	}
+	public MyEdgeNode getFirstEdge() {
+		return firstEdge;
+	}
+	public void setFirstEdge(MyEdgeNode firstEdge) {
+		this.firstEdge = firstEdge;
+	}
+	
+	
+
+}
