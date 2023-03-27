332e129888f815dc60bf8b46437f3e9f37e8e765
karina.skirmante <karina.krinkele@venta.lv>
2023-03-27 11:04:31 +0300

    Add: EdgeNode variables and set and get

--- src/datastr/MyEdgeNode.java
+++ src/datastr/MyEdgeNode.java
@@ -1,5 +1,44 @@
 package datastr;
 
 public class MyEdgeNode {
+	private int indexOfVertice;
+	private float weigth;
+	private MyEdgeNode next = null;
+	
+	
+	public int getIndexOfVertice() {
+		return indexOfVertice;
+	}
+	public void setIndexOfVertice(int inputIndexOfVertice) {
+		if(inputIndexOfVertice >= 0) {
+			indexOfVertice = inputIndexOfVertice;
+		}
+		else
+		{
+			indexOfVertice = -1;
+		}
+	}
+	public float getWeigth() {
+		return weigth;
+	}
+	public void setWeigth(float inputWeigth) {
+		if(inputWeigth > 0 && inputWeigth < 43000) {
+			weigth = inputWeigth;
+		}
+		else
+		{
+			weigth = 0;
+		}
+	}
+	public MyEdgeNode getNext() {
+		return next;
+	}
+	public void setNext(MyEdgeNode next) {
+		this.next = next;
+	}
+	
+	
+	
+	
 
 }
