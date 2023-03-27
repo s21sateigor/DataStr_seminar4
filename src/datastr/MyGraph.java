81dab57b475b35e1a9a2883201b6a7fbc3e56372
karina.skirmante <karina.krinkele@venta.lv>
2023-03-27 11:35:03 +0300

    Add: MyGraph class with isEmpty, isFull, howManyelements and  increaseArray

--- src/datastr/MyGraph.java
+++ src/datastr/MyGraph.java
@@ -19,5 +19,38 @@ public class MyGraph<T> {
 
 		graphElements = new MyVerticeNode[arraySize];
 	}
+	
+	//TODO isFull, isEmpty, howManyElements and increaseArray
+	public boolean isEmpty() {
+		return (elementCounter == 0);
+	}
+
+	public boolean isFull() {
+		return (elementCounter == arraySize);
+	}
+
+	public int howManyElements() {
+		return elementCounter;
+	}
+
+	// 1. create a definition of function increaseArray
+	private void increaseArray() {
+		// 2. calculate the new size of the new array
+		int newArraySize = (arraySize > 100) ? (int) (arraySize * 1.5) : arraySize * 2;
+
+		// 3. create the new array
+		MyVerticeNode[] newElements =  new MyVerticeNode[newArraySize];
+		// 4. copy all elements from the old array to the new array
+		for (int i = 0; i < elementCounter; i++) {
+			newElements[i] = graphElements[i];
+		}
+		// 5. change reference to the new array and it's size
+		graphElements = newElements;
+		arraySize = newArraySize;
+	}
+	
+	
+	
+	
 
 }
